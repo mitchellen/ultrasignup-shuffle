2279dfd47ceb997d24e0b96c944e137cb70d35bf
mitchell <mitchell@penguin>
2020-10-17 20:48:36 -0400

    Getting class working for race/event

--- ultraparticipant.py
+++ ultraparticipant.py
@@ -1,10 +1,8 @@
 import re
 import csv
-import pandas as pd
 import time
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-#import matplotlib
 class Runner:
    '''Doc
     '''
@@ -12,7 +10,7 @@ class Runner:
       #age,number of races, url
       #self.array = array
       self.url = url
-      races = []
+      events = []
       self.first = array[0].split(' ')[0]
       self.last = array[0].split(' ')[1]
       self.age  = self.just_age(array[0].split(' ')[-1])
@@ -24,14 +22,38 @@ class Runner:
        return (age[1:])
 
 class Race:
-   '''Get them races classified
-   '''
-   def __init__(self,name,distance,date,time,age):
-      self.name = name
-      self.distance = distance
-      self.date = date
-      self.time = time
-      self.age = age
+   def __init__(self, array):
+      self.name = array[0].split('-')[0].strip()
+      self.distance = array[0].split('-')[1].strip()
+      self.date = array[1].strip()
+      #these are DNF/DNS and Future races
+      if len(array) <= 2:
+          if array[0].split('-')[-1].strip() == 'DNS' or  array[0].split('-')[-1].strip() == 'DNF':
+             self.status = array[0].split('-')[-1].strip()
+             self.distance = array[0].split('-')[-3].strip()
+             self.location  = array[0].split('-')[-2].strip()
+             #self.weather = ?
+          else:
+             self.distance = array[0].split('-')[-2].strip()
+             self.status = 'Future'
+      # these are just odd cases
+      elif len(array) < 4:
+         self.endtime = array[2]
+         self.distance = array[0].split('-')[-2].strip()
+         self.location  = array[0].split('-')[-1].strip()
+         self.status = 'Complete'
+         #self.weather = ?
+         #pace = get_pace(endtime, dist)
+      #most races completed go here
+      else:
+         self.dist = array[0].split('-')[-2].strip()
+         self.endtime = array[3]
+         self.age = array[4].split(': ')[1]
+         self.location  = array[0].split('-')[-1].strip()
+         self.status = 'Complete'
+         #self.weather = ?
+        # pace = get_pace(endtime, dist)
+
 
 
 #functions
@@ -62,62 +84,30 @@ def get_pace(dur, dis):
 with open('out.txt', mode ='r')as file:
    time.sleep(5)
    output = csv.reader(file)
-   for u in output:
+   notlist = []
+   for l in output: #load entire csv into vari to get better data and num properties
+      notlist.append(l)
+   for u in notlist:
       #URL = 'https://ultrasignup.com/results_participant.aspx?fname=Cera&lname=Jones'
       #URL = 'https://ultrasignup.com/results_participant.aspx?fname=William&lname=Connell'
       URL = 'https://ultrasignup.com' + u[0]
-      # try chrome if it gets too gross
       opts = Options()
       opts.add_argument('--headless')
-      # print(opts)
       browser = Firefox(options=opts)
       browser.get(URL)
-      # There is a wwird thing that all race results show up in the same pot despite multiple participants
-      # How do I sort this out?
       runners = []
       acchead = browser.find_elements_by_class_name('accordion-heading')
       accordion = browser.find_elements_by_class_name('accordion-content')
       for num, runner in enumerate(acchead, start=0):
-         # rundata = {}
-         # runarray = runner.text.split('\n')
-         # runhash = {'Age': runarray[0].split(' ')[-1], 'Number of Races': runarray[1].split(' ')[0], 'URL': URL}
-         # rundata.update(runhash)
          runarray = runner.text.split('\n')
          run = Runner(runarray, URL)
          races = accordion[num].find_elements_by_class_name('rowlines')
-         event = []
-      for r in races:
-         iss = ''
-         # How do I find DNS,DNF,Future
-         race = r.text.split('\n')
-         if len(race) < 3:
-            if race[0].split('-')[-1].strip() == 'DNS' or  race[0].split('-')[-1].strip() == 'DNF':
-               iss = race[0].split('-')[-1].strip()
-               dist = race[0].split('-')[-3].strip()
-               endtime = iss
-            else:
-               dist = race[0].split('-')[-2].strip()
-               age = ''
-               endtime = 'Future'
-               racedict = {"Distance": dist, "Date": race[1], "Time": endtime}
-         elif len(race) < 4:
-            endtime = race[2]
-            age = ''
-            dist = race[0].split('-')[-2].strip()
-            pace = get_pace(endtime, dist)
-            racedict = {"Distance": dist, "Date": race[1], "Time": endtime, "Pace": pace, 'Age': age}
-         else:
-            dist = race[0].split('-')[-2].strip()
-            endtime = race[3]
-            age = race[4].split(': ')[1]
-            pace = get_pace(endtime, dist)
-            racedict = {"Distance": dist, "Date": race[1], "Time": endtime, "Pace": pace , 'Age': age}
-            event.append(racedict)
-            rundata.update({'Events': event})
-            #print(racedict)
-      runners.append(rundata)
-      df = pd.DataFrame(runners) #< broke
-      df.to_csv("out.csv", mode='a', header=False)
-      print(runners)
+         print(run.url)
+         for r in races:
+            racelist = r.text.split('\n')
+#            print(race)
+            obj = Race(racelist)
+            # add these to run.event.append(obj)
+            print(vars(obj)) # this is debug/crutch
       browser.close()
       #find out why pace is fucked
